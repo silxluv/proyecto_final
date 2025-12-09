2ef4261fbb19bb8245f06df42738cbb7e29622a8
silxluv <silviayuuki@outlook.com>
2025-12-08 18:02:12 -0600

    Programa para acceder con usuario y contraseña especificos

---
+++ proyectoFinal/proyectoFinal/MainForm.cs
@@ -0,0 +1,86 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Elia Martha
+ * Fecha: 08/12/2025
+ * Hora: 02:40 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace proyectoFinal
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			txtFechaActual.Text=DateTime.Now.ToString("dd/MM/yyyy");
+		}
+		
+		void TxtUsuarioEnter(object sender, EventArgs e)
+		{
+			if(txtUsuario.Text=="USUARIO")
+			{
+				txtUsuario.Text="";
+				txtUsuario.ForeColor=Color.LightGray;
+			}
+		}
+		
+		void TxtUsuarioLeave(object sender, EventArgs e)
+		{
+			if(txtUsuario.Text=="USUARIO")
+			{
+				txtUsuario.Text="";
+				txtUsuario.ForeColor=Color.DimGray;
+			}
+		}
+		
+		void TxtContraseñaEnter(object sender, EventArgs e)
+		{
+			if(txtContraseña.Text=="CONTRASEÑA")
+			{
+				txtContraseña.Text="";
+				txtContraseña.ForeColor=Color.LightGray;
+				txtContraseña.UseSystemPasswordChar=true;
+			}
+		}
+		
+		void TxtContraseñaLeave(object sender, EventArgs e)
+		{
+			if(txtContraseña.Text=="CONTRASEÑA")
+			{
+				txtContraseña.Text="";
+				txtContraseña.ForeColor=Color.DimGray;
+				txtContraseña.UseSystemPasswordChar=false;
+			}
+		}
+		
+		void BtnAccederClick(object sender, EventArgs e)
+		{
+			if(txtUsuario.Text=="usuariobonito" | txtContraseña.Text=="admin123")
+			{
+				MessageBox.Show("Bienvenido/a");
+			}else{
+				MessageBox.Show("Usuario o contraseñas incorrectos");
+			}
+		}
+	}
+}
---
+++ proyectoFinal/proyectoFinal/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Elia Martha
+ * Fecha: 08/12/2025
+ * Hora: 02:40 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Windows.Forms;
+
+namespace proyectoFinal
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
